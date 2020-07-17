91dccc85493d77188e953263b34efdd6b0d2df2c
Keara Polovick <kearapolovick@Kearas-MacBook-Pro.local>
2020-07-17 11:52:50 -0400

    Initial files

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("alert").addEventListener("click", function () {
+    alert("keara.polovick@gmail.com");
+  });
+  document.getElementById("alert2").addEventListener("click", function () {
+    alert("(315)-708-5580");
+  });  
+  
+  
